c2ab8fbed86a255188ccbdc6c360dcaba1b4bc82
willdunlop <william.dunlop.1993@gmail.com>
2016-08-25 17:27:19 +1000

    25/08 -v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
   get 'pages/about'
   get 'pages/work'
   get 'pages/contact'
   get 'pages/links'
+  # get 'pages/hilltown'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
